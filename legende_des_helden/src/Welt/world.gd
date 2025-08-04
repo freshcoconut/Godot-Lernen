16c0758376d3201ce5f3898e6abdab54c89e2e55
coconut <gavinguiccy@qq.com>
2025-08-04 14:21:24 +0800

    Update .editorconfig, .gitattributes, .gitignore, and 173 more files

---
+++ legende_des_helden/src/Welt/world.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+@onready var tile_map: TileMap = $TileMap
+@onready var camera_2d: Camera2D = $Spieler/Camera2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var used := tile_map.get_used_rect().grow(-1)
+	var tile_size := tile_map.tile_set.tile_size
+	
+	#position: the left top point of rectangle
+	#end: the right bottom point of rectangle
+	camera_2d.limit_top = used.position.y * tile_size.y
+	camera_2d.limit_left = used.position.x * tile_size.x
+	camera_2d.limit_bottom = used.end.y * tile_size.y
+	camera_2d.limit_right = used.end.x * tile_size.x
+	
+	camera_2d.reset_smoothing()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
